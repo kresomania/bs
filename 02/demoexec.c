b9322069f4f4719eb1460e13a766d6cc2a9394c3
kresomania <kresimir.bagaric@gmail.com>
2012-10-07 21:38:40 +0200

    chain (&fork) Aufgabe

---
+++ 02/demoexec.c
@@ -0,0 +1,25 @@
+/* Nach: An Introductory 4.3BSD IPC Tutorial, S. Sechrest */
+/* 03.12.04 / gle */
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main () {
+  pid_t pid;
+  int status;
+
+  if (fork() == 0) {
+    printf ("Child executing \"/bin/date\"\n");
+    printf ("PID(if): %d\n", getpid());
+    execl ("/bin/date", "date", 0);
+    printf ("This should not happen!\n");
+  }
+  printf ("Parent waiting for child's dead\n");
+  pid=wait (&status);
+  printf ("Status:    %d\n", pid);
+  printf ("PID:       %d\n", getpid());
+  printf ("Parent: Child just died.\n");
+  exit(0);
+}
