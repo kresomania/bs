94f9d8bcde435639c1b398145e9e500691788d67
kresomania <kresimir.bagaric@gmail.com>
2012-10-17 19:23:23 +0200

    myDriver: hello.c added

---
+++ driverUebung/.hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/kresi/bs/bs/driverUebung/hello.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.2.0-31-generic-pae/scripts/module-common.lds --build-id  -o /home/kresi/bs/bs/driverUebung/hello.ko /home/kresi/bs/bs/driverUebung/hello.o /home/kresi/bs/bs/driverUebung/hello.mod.o
---
+++ driverUebung/.tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/home/kresi/bs/bs/driverUebung/hello.ko
+/home/kresi/bs/bs/driverUebung/hello.o
